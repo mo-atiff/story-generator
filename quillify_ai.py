1f5ad5e8fca8a5a908d38ac16e8ecf4c3845e4b6
Atif <91662836+mo-atiff@users.noreply.github.com>
2023-12-28 22:49:06 +0530

    Update quillify_ai.py

--- quillify_ai.py
+++ quillify_ai.py
@@ -284,7 +284,7 @@ def main():
         else:
             bytes_path = speak_text(story, voices[1])
 
-	st.write("BYTES PATH : ", bytes_path)
+        st.write("BYTES PATH : ", bytes_path)
         with open(bytes_path, "rb") as mp3_file:
             aud_bytes = mp3_file.read()
 
