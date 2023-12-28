3f8a27cdf9eefd7842af32dcf95ea91c6b7ebc34
Atif <91662836+mo-atiff@users.noreply.github.com>
2023-12-28 19:02:51 +0530

    Add files via upload

---
+++ markdowns/ele.py
@@ -0,0 +1,16 @@
+sides = '''
+        🌟✨ *Welcome to our Enchanting Creative Haven!* 🎉
+
+        *Dive into the magical world of storytelling with our spellbinding app!*
+
+        📜 *Simply sprinkle your story ideas, and watch the magic unfold before your eyes.*
+
+        💫 *We'll craft a short and captivating narrative, adorned with dazzling images created just for your words.*
+
+        🌈 *But that's not all! A special touch awaits - an AI companion will melodiously recite your tale, turning your imagination into an enchanting experience.*
+
+        💬 *Express yourself through the power of words, mesmerizing images, and the soothing voice of our AI narrator.*
+
+        🎨 *Let your creativity soar as we embark on a whimsical storytelling adventure together!* 🚀🌌
+'''
+
