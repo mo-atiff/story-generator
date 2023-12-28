4d72cc89dd18d741ed8026b3e3544b4de8098318
Atif <91662836+mo-atiff@users.noreply.github.com>
2023-12-28 22:32:26 +0530

    Update quillify_ai.py

--- quillify_ai.py
+++ quillify_ai.py
@@ -183,12 +183,11 @@ def speak_text(text, voice, rate=150):
 
 
     download_dir = 'dir_mp3'
-    st.write(os.getcwd())
-    os.path.join(os.getcwd(), download_dir)
-    # os.makedirs(download_dir, exist_ok=True)
-    st.write(os.getcwd.listdir('/'))
+    full_path = os.path.join(os.getcwd(), download_dir)
+    os.makedirs(full_path, exist_ok=True)
     # file_path = os.path.join(download_dir, "my_audio.mp3") 
-    file_path = os.path.join(download_dir, "my_audio.wav") 
+    file_path = os.path.join(full_path, "my_audio.wav")
+    # file_path = os.path.join(download_dir, "my_audio.wav") 
     st.write("File path : ", file_path)
 
     engine.save_to_file(text, file_path)
