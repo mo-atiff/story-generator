c1a65098b696ff9cf76e3c7d98cee3916bff1d47
Atif <91662836+mo-atiff@users.noreply.github.com>
2023-12-28 19:01:35 +0530

    Add files via upload

---
+++ demos/gallery.py
@@ -0,0 +1,67 @@
+import streamlit as st
+from markdowns import ele
+import os
+import shutil
+
+if 'links' not in st.session_state:
+    st.session_state.links = None
+
+if 'user_links' not in st.session_state:
+    st.session_state.user_links = None
+
+if 'prompt_sess' not in st.session_state:
+    st.session_state.prompt_sess = []
+
+vids_links = ["C:\\Users\\Dell\\Downloads\\mysterious_island.mp4", "C:\\Users\\Dell\\Downloads\\animal_world.mp4", 
+                "C:\\Users\\Dell\\Downloads\\venice.mp4", "C:\\Users\\Dell\\Downloads\\mars.mp4"]
+
+
+user_vids_links = []
+
+
+st.session_state.links = vids_links
+st.session_state.user_links = user_vids_links
+st.session_state.prompt_sess.append(st.session_state.userprompt)
+# journey to mysterious island, somewhere in pacific ocean - realistic
+# zootopia - cartoonish
+# Depict a story of, Young Boy gifts flower's to a girl in Venice and confesess his love to her - anime
+# A little boy's journey to mars with a home made rocket - realistic
+
+st.markdown("<h1 style='text-align: centre; color: yellow;'>Gallery</h1>", unsafe_allow_html=True)
+st.write("Vids generated through this app :)")
+
+direc = "C:\\Python3.10.1\\Lib\\site-packages\\QtDesigner\\VsCode Py\\story_gen\\dir_mp4"
+
+directory_path = os.listdir(direc)
+
+def side_bar_view():
+    return ele.sides
+
+side = side_bar_view()
+st.sidebar.write(side)
+
+
+if st.session_state.upload:
+    if os.path.isdir(direc):
+        for i in directory_path:
+            st.session_state.user_links.append(i)
+    else:
+        pass
+
+
+for i in range(0, len(st.session_state.links), 2):
+    col1, col2 = st.columns(2)
+    col1.video(st.session_state.links[i])
+    if i + 1 < len(st.session_state.links):
+        col2.video(st.session_state.links[i + 1])
+
+st.markdown("<h1 style='text-align: centre; color: yellow;'>Your videos will show up here</h1>", unsafe_allow_html=True)
+
+for i in range(0, len(st.session_state.user_links), 2):
+    col1_, col2_ = st.columns(2)
+    col1_.video(direc+'\\'+st.session_state.user_links[i])
+    if i + 1 < len(st.session_state.user_links):
+        col2_.video(direc+'\\'+st.session_state.user_links[i + 1])
+
+# shutil.rmtree("dir_png")
+
